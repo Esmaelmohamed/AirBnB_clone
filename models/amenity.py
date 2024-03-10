5e5592f98b98b7b39cb700f2ef3ca1022dc85d40
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-09 17:43:45 -0800

    delete all

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-from models.base_model import BaseModel
-
-class Amenity(BaseModel):
-    """Amenity class inherits from BaseModel."""
-    
-    def __init__(self):
-        """Initialize Amenity instance."""
-        super().__init__()
-        self.name = ""
--- models/state.py
+++
@@ -1,9 +0,0 @@
-from models.base_model import BaseModel
-
-class State(BaseModel):
-    """State class inherits from BaseModel."""
-    
-    def __init__(self):
-        """Initialize State instance."""
-        super().__init__()
-        self.name = ""
